4bb1191442146b348ff4a40fb544ebc2fa2e3e3b
Savva Kodeikin <savva.kodeikin@gmail.com>
2021-08-22 16:06:19 +0200

    Take products from the DB

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,43 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import mongoose from "mongoose";
+import Product from "../models/productModel.js";
+
+const router = express.Router();
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// @desc    Fetch a single product
+// @route   GET /api/products/:id
+// @access  Public
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+      const product = await Product.findById(req.params.id);
+
+      if (product) {
+        res.json(product);
+      } else {
+        res.status(404).json({
+          message: "Product not found",
+        });
+      }
+    } else {
+      res.status(404).json({
+        message: "Incorrect id",
+      });
+    }
+  })
+);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 connectDB();
 const app = express();
 
+app.use("/api/products", productRoutes);
+
 const PORT = process.env.PORT || 5000;
 const ENV = process.env.NODE_ENV;
-
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 
 app.listen(
   PORT,
